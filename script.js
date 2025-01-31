098f1c934f7c94b6c9f1fc2e28b6dbee702fcdd5
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-01-31 21:49:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.getElementById('myForm').addEventListener('submit',function(event)){
+	event.preventDefault();
+
+	var firstName=document.getElementById('firstName').value;
+
+	var message = 'First Name: ' + firstName;
+
+	alert(message);
+}
