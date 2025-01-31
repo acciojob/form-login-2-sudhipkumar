9256e9a8089bd158b8c6d6be1669b5aee1a46edd
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-01-31 21:51:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 //your JS code here. If required.
-document.getElementById('myForm').addEventListener('submit',function(event)){
-	event.preventDefault();
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-	var firstName=document.getElementById('firstName').value;
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var phoneNumber = document.getElementById('phoneNumber').value;
+    var emailId = document.getElementById('emailId').value;
 
-	var message = 'First Name: ' + firstName;
+    var message = 'First Name: ' + firstName + '\n' + 
+                  'Last Name: ' + lastName + '\n' + 
+                  'Phone Number: ' + phoneNumber + '\n' + 
+                  'Email ID: ' + emailId;
 
-	alert(message);
-}
+    alert(message);
+});
